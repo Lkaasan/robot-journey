88ae0fcdca7ab0eb8969833cae68b95d63d34c84
Lukas Kaas Andesen <49850026+Lkaasan@users.noreply.github.com>
2024-05-13 22:05:37 +0100

    Finished dropping samples

--- robot.py
+++ robot.py
@@ -7,22 +7,13 @@ sequence = sys.argv[1]
 fuel = 100
 weight = 5
 fuel_empty = False
+fuel_consumed = 0
 destination = 0
+
 x = 0
 y = 0
-samples = []
 
-class Samples:
-    
-    def __init__(self, coordinates, weight):
-        self.coordinates = coordinates
-        self.weight = weight
-        
-    def get_coorindates(self):
-        return self.coordinates
-    
-    def get_weight(self):
-        return self.weight
+dropped_samples = {}
     
 def convert_sequence(sequence):
     locations_queue = []
@@ -40,12 +31,7 @@ def get_fuel_consumption(weight):
 def get_distance(location1, location2, weight):
     return (abs((location1[0] - location2[0])) + abs(location1[1] - location2[1])) * get_fuel_consumption(weight)
 
-def drop_samples():
-    samples.append(Samples((x, y), weight - 5))
-
-
 print("sequence:", sequence)
-
 
 locations_queue = convert_sequence(sequence)
 
@@ -59,11 +45,16 @@ while locations_queue:
         continue
     
     if get_distance((x, y), location, weight) + get_distance(location, (0, 0), weight + 10) > fuel:
-        if get_distance((x, y), location, 5) + get_distance(location, (0, 0), 15) <= fuel:
-            print("dropping samples")
-            drop_samples()
-            locations_queue.append((x, y))
+        if location in dropped_samples:
+            addedweight = dropped_samples.get(location)
+        else:
+            addedweight = 5
+        if get_distance((x, y), location, addedweight) + get_distance(location, (0, 0), addedweight + 10) <= fuel:
+            print("Dropping Samples")
             next_location = locations_queue.pop(0)
+            locations_queue.insert(-1, (x, y))
+            dropped_samples[(x, y)] = weight - 5
+            weight = 5 
         else:
             next_location = (0, 0)
     else:
@@ -78,11 +69,16 @@ while locations_queue:
                 print("Refueling!")
             else:
                 destination += 1
-                weight += 10
+                if (x, y) in dropped_samples:
+                    weight += dropped_samples.get((x, y))
+                    del dropped_samples[(x, y)]
+                else:
+                    weight += 10
                 print("Arrived at Destination ", destination)
+                print("Fuel: ", fuel)
             arrived = True
         else:
-            if fuel < 1:
+            if fuel <= 0:
                 print("Ran out of fuel!")
                 fuel_empty = True
                 break
@@ -96,6 +92,9 @@ while locations_queue:
                     y += 1
                 elif y > next_location[1]:
                     y -= 1
-            print("Fuel: ", fuel)        
-            fuel -= get_fuel_consumption(weight)
+            fuel_used = get_fuel_consumption(weight)     
+            fuel -= fuel_used
+            fuel_consumed += fuel_used
             print(f"> {x};{y}")
+
+print("Total Fuel Used: " , fuel_consumed)
